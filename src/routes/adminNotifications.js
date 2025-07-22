780535c69aef1e2a3e4c3c5213ee96742af86b66
Thanh23-7 <106516270+Thanh23-7@users.noreply.github.com>
2025-07-23 00:02:32 +0700

    upate notifications

--- src/routes/adminNotifications.js
+++ src/routes/adminNotifications.js
@@ -1,31 +1,9 @@
-const express = require('express');
+import express from 'express';
+import { sendNotificationToAll } from '../controllers/notificationController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+
 const router = express.Router();
-const Notification = require('../models/Notification');
-const { verifyAdmin } = require('../middlewares/authMiddleware'); // middleware kiểm tra admin
 
-router.post('/notifications', verifyAdmin, async (req, res) => {
-  try {
-    const { content, recipients } = req.body;
+router.post('/notifications', authMiddleware.authenTokenCookie, authMiddleware.isAdmin, sendNotificationToAll);
 
-    if (!content) {
-      return res.status(400).json({ message: 'Nội dung không được để trống' });
-    }
-
-    // recipients = [] nghĩa gửi tất cả
-    const notification = new Notification({
-      content,
-      recipients,
-    });
-
-    await notification.save();
-
-    // TODO: phát thông báo qua socket cho khách hàng nhận
-
-    return res.status(201).json({ message: 'Thông báo đã được gửi' });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Lỗi server' });
-  }
-});
-
-module.exports = router;
+export default router;
